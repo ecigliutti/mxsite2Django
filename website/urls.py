b5b651fcb670e2d67eed4c937e9bfbcb32d6323e
Eduardo Cigliutti <eacigliutti@gmail.com>
2023-10-04 11:19:26 -0400

    Second Load

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from . import views #the period means from inside this directory (website)
 
 urlpatterns = [
-    path('', views.home, name="home"),
-]
+    
+    path('', views.home, name="home"), #the path '' means root path, views references views.py file
+    path('home.html', views.contact, name="contact"), #the path 'not empty' means path to specific file, views references views.py file
+
+] 
--- website/views.py
+++ website/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.core.mail import send_mail #imports django email system
 
 # Create your views here.
 def home(request):
 	return render(request, 'home.html', {})
+
+def contact(request):
+#{} is a context dictionary and allow to pass data from the back to the front end
+	if  request.method == "POST":
+		message_name = request.POST['message-name'] 
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		#Send mail function
+		send_mail(
+			message_name, # subject
+			message, # message
+			message_email, # from email
+			['eacigliutti@gmail.com'], # to email
+			fail_silently=False,
+			)
+		return render(request, 'contact', {'message_name':message_name})    
+	else:
+
+		return render(request, 'contact', {})
+
